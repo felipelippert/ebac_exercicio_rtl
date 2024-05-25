5914a30c00425b380a62adab11640ca321cfa385
Felipe Valcarenghi Lippert <felipe.v.lippert@gmail.com>
2024-05-25 23:01:08 +1000

    finished

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+    test('Deve adicionar dois comentários corretamente', () => {
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+
+        // Typing first comment
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        })
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+
+        // Typing second comment
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('comment-button'))
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2)
+
     });
 });
